ab7ecd4cf0e417c31a8ef892c84fbb6b4fb2e273
Zeenat Parveen <zenizee383@gmail.com>
2025-02-10 03:35:53 +0500

    login

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   console.log("hello ");
   res.send("hey i am back");
 });
+app.get("/login", (req, res) => {
+  console.log("hello ");
+  res.send("hey i am back with login");
+});
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`); // log the server is running on the specified port
 });
